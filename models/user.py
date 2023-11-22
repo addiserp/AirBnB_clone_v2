5b12f4aa63653591b220f35d9667ddf88550e765
Mikias Gedlu <addiserp@gmail.com>
2023-11-22 17:05:58 +0300

    0x02. AirBnB clone - MySQL setup updated

--- models/user.py
+++ models/user.py
@@ -1,11 +1,34 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel
+""" This module contains a class User"""
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+class User(BaseModel, Base):
+    """ A representation of a user
+    User inherits from BaseModel and Base.
+    """
+
+    if models.storage_t == 'db':
+        __tablename__ = 'users'
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        places = relationship("Place", backref="user")
+        reviews = relationship("Review", backref="user")
+
+    else:
+        first_name = ""
+        last_name = ""
+        email = ""
+        password = ""
+
+    def __init__(self, *args, **kwargs):
+        """It initializes user class"""
+
+        super().__init__(*args, **kwargs)
