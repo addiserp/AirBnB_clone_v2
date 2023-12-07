143b2a03cbe5d84a3d094bf0ca86e76a26dd89f2
Mikias Gedlu <addiserp@gmail.com>
2023-12-07 15:53:36 +0300

    0. Prepare your web servers 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,17 +8,19 @@
 
 from datetime import datetime
 from fabric.api import local
-from os.path import isdir
 
 
 def do_pack():
     """It generates a tgz archive"""
     try:
-        date = datetime.now().strftime("%Y%m%d%H%M%S")
-        if isdir("versions") is False:
-            local("mkdir versions")
-        file_name = "versions/web_static_{}.tgz".format(date)
-        local("tar -cvzf {} web_static".format(file_name))
-        return file_name
+        """Creates archive from web_static directory"""
+        local("mkdir -p versions")
+        file = 'versions/web_static_{}.tgz'\
+            .format(datetime.strftime(datetime.now(), "%Y%m%d%I%M%S"))
+        comp = 'tar -cvzf {} web_static'.format(file)
+        tar_file = local(comp)
+        if tar_file.failed:
+            return None
+        return file
     except Exception:
         return None
