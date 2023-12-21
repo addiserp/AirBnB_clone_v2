f43dfec56917462abf34aa2c51a2b1ac035a6d5f
Mikias Gedlu <addiserp@gmail.com>
2023-12-21 17:22:28 +0300

    0x04. AirBnB clone - Web framework 4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+# a script that starts a Flask web application:
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/number/<n>', strict_slashes=False)
+def isinteger(n):
+    """this will display “n is number ” only if n is an integer"""
+    if (type(n) is int):
+        return 'n is number'
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythoniscool(text="is cool"):
+    """
+        this will display “Python ” and followed by the value of input variable
+    """
+    return 'Python ' + text.replace('_', ' ')
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """this will display “C ” and followed by the value of input variable"""
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hello_world():
+    """this will return HBNB!"""
+    return "HBNB!"
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """this will return Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
