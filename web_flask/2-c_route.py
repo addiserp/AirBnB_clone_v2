bcd3ba28ac4e3b40d24a83a08837d6918c7b191b
Mikias Gedlu <addiserp@gmail.com>
2023-12-21 17:13:24 +0300

    0x04. AirBnB clone - Web framework 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# a script that starts a Flask web application:
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """this will display “C ” and followed by the value of input variable"""
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hello_world():
+    """this will return HBNB!"""
+    return "HBNB!"
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """this will return Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
