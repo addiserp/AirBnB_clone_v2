19994a3a388ebf639a491c43ff773212058bcc51
Mikias Gedlu <addiserp@gmail.com>
2023-12-27 17:34:53 +0300

    0x04. AirBnB clone - Web framework hotfix2

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -7,6 +7,13 @@ from flask import Flask
 app = Flask(__name__)
 
 
+@app.route('/', strict_slashes=False)
+def index():
+    """
+        this will return Hello HBNB!
+    """
+    return 'Hello HBNB!'
+
 @app.route('/hbnb', strict_slashes=False)
 def index():
     """
