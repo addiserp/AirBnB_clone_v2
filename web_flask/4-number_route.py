d64b33543bdb56e7d7346b2cd85349f184cde162
Mikias Gedlu <addiserp@gmail.com>
2023-12-27 17:45:15 +0300

    0x04. AirBnB clone - Web framework hotfix5

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,20 +1,21 @@
-#!/usr/bin/env bash
-# a script that starts a Flask web application:
+#!/usr/bin/python3
+"""
+    a script that starts a Flask web application:
+"""
 
 from flask import Flask
-
 app = Flask(__name__)
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def isinteger(n):
     """this will display “n is a number ” only if n is an integer"""
-    return "{} is a number".format(n)
+    return '{} is a number'.format(n)
 
 
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def pythoniscool(text="is cool"):
+def pythoniscool(text='is cool'):
     """
         this will display “Python ” and followed by the value of input variable
     """
@@ -27,16 +28,16 @@ def cisfun(text):
     return 'C ' + text.replace('_', ' ')
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hello_world():
     """this will return HBNB!"""
-    return "HBNB!"
+    return 'HBNB!'
 
 
-@app.route("/", strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello():
     """this will return Hello HBNB!"""
-    return "Hello HBNB!"
+    return 'Hello HBNB!'
 
 
 if __name__ == '__main__':
