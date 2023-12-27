338c33f12c7f6e7ee33f8c33ce1f954396898a95
Mikias Gedlu <addiserp@gmail.com>
2023-12-27 17:48:09 +0300

    0x04. AirBnB clone - Web framework hotfix6

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,29 +1,22 @@
-#!/usr/bin/env bash
-# a script that starts a Flask web application:
-# retun HTML tag is n is integer.
+#!/usr/bin/python3
+"""
+    a script that starts a Flask web application:
+    retun HTML tag is n is integer.
+"""
+
 from flask import Flask, render_template
-
 app = Flask(__name__)
-
-
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def isinteger_template(n):
-    """
-        this will display display a HTML page only if
-        n is an integer:
-    """
-    return render_template('5-number.html', n=n)
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def isinteger(n):
     """this will display “n is a number ” only if n is an integer"""
-    return "{} is a number".format(n)
+    return '{} is a number'.format(n)
 
 
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def pythoniscool(text="is cool"):
+def pythoniscool(text='is cool'):
     """
         this will display “Python ” and followed by the value of input variable
     """
@@ -36,16 +29,16 @@ def cisfun(text):
     return 'C ' + text.replace('_', ' ')
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hello_world():
     """this will return HBNB!"""
-    return "HBNB!"
+    return 'HBNB!'
 
 
-@app.route("/", strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello():
     """this will return Hello HBNB!"""
-    return "Hello HBNB!"
+    return 'Hello HBNB!'
 
 
 if __name__ == '__main__':
