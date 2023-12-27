8c56df0de0b0eaf1d4666f2031ed3eff2fb575b3
Mikias Gedlu <addiserp@gmail.com>
2023-12-27 17:41:51 +0300

    0x04. AirBnB clone - Web framework hotfix4

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
-# a script that starts a Flask web application:
+""" a script that starts a Flask web application:"""
 
 from flask import Flask
-
 app = Flask(__name__)
 
 
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def pythoniscool(text="is cool"):
+def pythoniscool(text='is cool'):
     """
         this will display “Python ” and followed by the value of input variable
     """
@@ -21,16 +20,16 @@ def cisfun(text):
     return 'C ' + text.replace('_', ' ')
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hello_world():
     """this will return HBNB!"""
-    return "HBNB!"
+    return 'HBNB!'
 
 
-@app.route("/", strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello():
     """this will return Hello HBNB!"""
-    return "Hello HBNB!"
+    return 'Hello HBNB!'
 
 
 if __name__ == '__main__':
