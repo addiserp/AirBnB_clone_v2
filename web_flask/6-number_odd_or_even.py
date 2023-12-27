e7c5c8464dcd457b05b5c7915a5089329c14f2c0
Mikias Gedlu <addiserp@gmail.com>
2023-12-27 17:52:40 +0300

    0x04. AirBnB clone - Web framework hotfix7

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,15 +1,17 @@
-#!/usr/bin/env bash
-# a script that starts a Flask web application:
-# retun HTML tag is n is integer.
-from flask import Flask, render_template
+#!/usr/bin/python3
+"""
+    a script that starts a Flask web application:
+    retun HTML tag is n is integer.
+"""
 
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def odd_or_even(n):
     """
-    will return H1 tag: “Number: n is even|odd” inside the tag BODY
+        will return H1 tag: “Number: n is even|odd” inside the tag BODY
     """
     if (n % 2 == 0):
         nvalue = "{} is even".format(n)
@@ -20,23 +22,20 @@ def odd_or_even(n):
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def isinteger_template(n):
-    """
-        this will display display a HTML page only if
-        n is an integer:
-    """
+def numbersandtemplates(n):
+    """It displays a HTML page only if n is an integer"""
     return render_template('5-number.html', n=n)
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def isinteger(n):
     """this will display “n is a number ” only if n is an integer"""
-    return "{} is a number".format(n)
+    return '{} is a number'.format(n)
 
 
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def pythoniscool(text="is cool"):
+def pythoniscool(text='is cool'):
     """
         this will display “Python ” and followed by the value of input variable
     """
@@ -49,16 +48,16 @@ def cisfun(text):
     return 'C ' + text.replace('_', ' ')
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hello_world():
     """this will return HBNB!"""
-    return "HBNB!"
+    return 'HBNB!'
 
 
-@app.route("/", strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello():
     """this will return Hello HBNB!"""
-    return "Hello HBNB!"
+    return 'Hello HBNB!'
 
 
 if __name__ == '__main__':
