108a8bc2aba6243bd7208878b12ff986f02ef023
Mikias Gedlu <addiserp@gmail.com>
2023-12-27 17:20:27 +0300

    0x04. AirBnB clone - Web framework hot1

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,16 @@
-#!/usr/bin/env bash
-# a script that starts a Flask web application:
-
+#!/usr/bin/python3
+"""
+It starts a Flask web application
+"""
 
 from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def index():
-    """this will return Hello HBNB!"""
-    return "Hello HBNB!"
-
+    """It returns Hello HBNB!"""
+    return 'Hello HBNB!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
