002457f9798eafbbffb824d4662df76d4509ee0c
Mikias Gedlu <addiserp@gmail.com>
2024-01-01 17:19:52 +0300

    0x04. AirBnB clone - Web framework 8

---
+++ web_flask/9-states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+    starts a Flask web application
+    To load all cities of a State:
+"""
+
+from flask import Flask, render_template
+from models import *
+from models import storage
+import models
+
+app = Flask("__name__")
+
+
+@app.teardown_appcontext
+def refresh(exception):
+        models.storage.close()
+
+
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<state_id>', strict_slashes=False)
+def states(state_id=None):
+    """will display the states and cities listed in order"""
+    states = storage.all("State")
+    if state_id is not None:
+        state_id = 'State.' + state_id
+    return render_template('9-states.html', states=states, state_id=state_id)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5001')
